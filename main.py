eb7c53392d24c3c9056888541a7d9eaceb16c9ef
houmenghang <houmenghang@huiye.com>
2021-03-26 14:10:13 +0800

    update

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from read_excel import load_data
 from video_download import get_video
 from video_to_audio import video_process
 
-video_output = r'\\192.168.1.100\data\houmenghang\test\video\海贼王'
-audio_output = r'\\192.168.1.100\data\houmenghang\test\audio\海贼王'
+video_output = r'\\192.168.1.100\data\houmenghang\test\video\巩汉林'
+audio_output = r'\\192.168.1.100\data\houmenghang\test\audio\巩汉林'
 
 if __name__ == "__main__":
     os.makedirs(video_output, exist_ok=True)
     os.makedirs(audio_output, exist_ok=True)
-    excel_file = 'data_list\海贼王.xlsx'
+    excel_file = 'data_list\小品节目单.xlsx'
     data = load_data(excel_file)
     for i in range(1, len(data)):
         video_path = os.path.join(
