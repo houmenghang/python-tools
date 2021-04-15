37da061099dfd1332f9b56fa8d636146cb8d357e
houmenghang <houmenghang@huiye.com>
2021-04-15 14:34:35 +0800

    video down load update

--- video_download.py
+++ video_download.py
@@ -1,6 +1,8 @@
 import os
 
-def get_video(output, filename, url):
-    shell = 'you-get --debug -o {} -O "{}" {}'.format(output, filename.replace(' ', ''), url)
+
+def get_video(output=None, filename=None, url=None):
+    #shell = 'you-get --debug -o {} -O "{}" {}'.format(output, filename.replace(' ', ''), url)
+    shell = 'you-get -o {} {}'.format(output, url.replace('&', '("&")'))
     os.system(shell)
     print('{} is done'.format(filename))
